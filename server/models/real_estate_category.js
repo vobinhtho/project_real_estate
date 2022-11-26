b57a2dac92f26807858e1c5a8362ce180168da36
Võ Bình Thơ <thovo2427@gmail.com>
2022-11-27 00:41:16 +0700

    Revert "Revert "Revert "feature a complete"""
    This reverts commit 75588c8e1bf03f15ee7a5a642f81a5d42cb04493.

--- server/models/real_estate_category.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-const real_estateCategorySchema = mongoose.Schema({
-    name_category:String,
-    description_category:String,
-})
-
-var real_estateCategory = mongoose.model("real_estate_category", real_estateCategorySchema);
-
-export default real_estateCategory;
